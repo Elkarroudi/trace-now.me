29b6e05bfe0094584223096c347863f2d38df5c3
Elkarroudi <amineelkarroudi04@proton.me>
2025-02-17 20:31:45 +0100

    Setup Spring Project

---
+++ src/main/java/com/elkarroudi/tracenow/TracenowApplication.java
@@ -0,0 +1,13 @@
+package com.elkarroudi.tracenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracenowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracenowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elkarroudi/tracenow/TracenowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elkarroudi.tracenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracenowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
